a9c524b33589520b5dc5849fb85de0b14a45a7a4
ezz khalaf <ezzeldeenawni@gmail.com>
2025-05-24 17:18:02 +0300

    chnaged from brouser router to hash

--- foodbank_frontend/src/main.jsx
+++ foodbank_frontend/src/main.jsx
@@ -4,17 +4,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter , HashRouter } from 'react-router-dom'
 import { AuthProvider } from './Context/AuthContext.jsx'
 import 'react-toastify/dist/ReactToastify.css';
 
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <HashRouter >
         <AuthProvider>
           <App />
+        
         </AuthProvider>
-      </BrowserRouter>     
+      </HashRouter>
+      {/* </BrowserRouter>      */}
   </StrictMode>
 )
